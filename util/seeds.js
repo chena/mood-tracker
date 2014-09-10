5f489d4cce00cad709262aa762c944971aecc2a4
Alice Chen <chiachun78@gmail.com>
2014-09-10 13:51:58 -0400

    Added more seed data and modified seeds script to include fname and date param

--- util/seeds.js
+++ util/seeds.js
@@ -1,24 +1,47 @@
 var fs = require('fs');
 var moment = require('moment');
 
-var generateMoodSeeds = function() {
+var generateMoodSeeds = function(startDate) {
     moods = ['happy', 'okay', 'unhappy'];
     dailyMoods = [];
 
-    for (var date = moment('2014-07-21'); !date.isAfter(moment()); date = date.add(1, 'd')) {
-        var mStr = JSON.stringify({
+    for (var date = moment(startDate); !date.isAfter(moment()); date = date.add(1, 'd')) {
+        dailyMoods.push({
             date: date.format('YYYY-MM-DD'),
             mood: moods[Math.floor(Math.random() * moods.length)]
-        }, null, 4);
-        dailyMoods.push(mStr);
+        });
     }
     return dailyMoods;
 };
 
-fs.writeFile('data.json', generateMoodSeeds(), function(err) {
-    if (err) {
-        console.error('something went wrong!');
-    } else {
-        console.log('file written!');
+var args = process.argv;
+if (args.length < 3) {
+    console.error('usage: node util/seeds.js <filename> [<start_date>]');
+    process.exit(1);
+}
+
+var base = 'util/data', 
+    fname = args[2],
+    startDate = args[3] ? args[3] : '2014-07-21';
+
+if (!moment(startDate).isValid()) {
+    console.error('invalid date: ' + startDate);
+    process.exit(1);
+}
+
+fs.stat(base, function(err, s) {
+    if (err && err.errno === 34) {
+        fs.mkdir('util/data', function(err) {
+            console.error('unable to create data directory');
+            process.exit(1);
+        });
     }
+    var data = generateMoodSeeds(startDate);
+    fs.writeFile(base + '/' + fname, JSON.stringify(data), function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log('file written!');
+    });
 });
+
