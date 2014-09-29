a6096988fdce4d32f5e26e965fa9e9739279e643
Alice Chen <chiachun78@gmail.com>
2014-09-29 16:54:18 -0400

    Configure hacker school provider in client side app instead of in satellizer

--- public/app.js
+++ public/app.js
@@ -1,7 +1,12 @@
 angular.module('MoodTracker', ['ngRoute', 'satellizer', 'mgcrea.ngStrap', 'angularCharts', 'ngSanitize'])
     .config(function($authProvider, $routeProvider) {
-        $authProvider.hackerschool({
-            clientId: '670e4651d9d490c58e536d7b59ca76b7927e445a4c4d65a1d7b6e1e9cf4fb2c5'
+        // configure Hacker School as a Oauth2 provider in Satellizer
+        $authProvider.oauth2({
+            name: 'hackerschool',
+            url: '/auth/hackerschool',
+            redirectUri: window.location.origin,
+            clientId: '670e4651d9d490c58e536d7b59ca76b7927e445a4c4d65a1d7b6e1e9cf4fb2c5',
+            authorizationEndpoint: 'https://www.hackerschool.com/oauth/authorize',
         });
 
         $routeProvider.when('/', {
