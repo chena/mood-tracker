7af14abd19c32026145c27f6cbddf15f320db916
Alice Chen <chiachun78@gmail.com>
2014-09-30 17:50:40 -0400

    Renaming in server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(requireSSL);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 
 /*
  |--------------------------------------------------------------------------
